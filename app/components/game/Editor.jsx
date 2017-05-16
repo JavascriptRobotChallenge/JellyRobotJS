94f9493fab336408c0fd0b0256e8f94013cc49b6
Sam Bakkila <sbakkila@gmail.com>
2017-05-15 20:54:52 -0400

    feat: sandbox user input

--- app/components/game/Editor.jsx
+++ app/components/game/Editor.jsx
@@ -7,21 +7,10 @@ import 'brace/mode/javascript';
 import 'brace/theme/monokai';
 
 var startingCode =
-`(function(){
-   function SubRobot(){
-       this.color = "red"
-    };
-
-  SubRobot.prototype = Object.create(RobotClass.prototype)
-  //ToDo: call functions with roomName and PlayerId so we know
-  // which robot to move
-  SubRobot.prototype.start = function(roomName, playerId){
+`SubRobot.prototype.start = function(roomName, playerId){
     this.walkForward(roomName, playerId)
     this.walkTowardOpponent(roomName, playerId)
-  }
-
-  return new SubRobot()
-})`
+  }`
 var inputCode = startingCode
 
 class Editor extends React.Component {
