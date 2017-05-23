08ba80f0784256bdacf05bcb26dc20dde5d854a6
tina <tina.lam516@gmail.com>
2017-05-23 19:54:18 -0400

    fix buttons and win/loss/tie pages

--- app/components/game/Loss.jsx
+++ app/components/game/Loss.jsx
@@ -1,24 +1,24 @@
 import React from 'react';
 import {Link} from "react-router"
-export default class Loss extends React.Component {
-  constructor(props) {
-    super(props);
-  }
-  render(){
-      return(
-          <div>
-        <div id="headerpicloss">
-          <div className="headertext">
-            <div className="col-xs-4">
-            <Link to={"/home"}>
-              <h1 className="resulttext">You've been defeated!</h1>
-              <button>Back Home</button>
-              </Link>
-            </div>
-          </div>
-          </div>
+import { Button } from 'react-bootstrap'
 
+const Loss = (props) => (
+    <div>
+      <div id="headerpicloss">
+        <div className="headertext">
+          <div className="col-xs-4">
+          <Link to={"/home"}>
+            <h1 className="resulttext">You've been defeated!</h1>
+              <Button
+                bsStyle="default"
+                bsSize="lg"
+              > Back Home
+              </Button>
+            </Link>
+          </div>
         </div>
-          )
-  }
-}
+      </div>
+    </div>
+)
+
+export default Loss
